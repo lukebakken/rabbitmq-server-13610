114b1f65d64550c6d219467515d4ea156bce854b
Luke Bakken <luke@bakken.io>
2025-03-25 08:41:41 -0700

    initial commit

---
+++ tls_client.erl
@@ -0,0 +1,27 @@
+-module(tls_client).
+
+-export([start/0, start/1]).
+
+start() ->
+    start("localhost", 636).
+
+start([Host, Port]) when is_atom(Host) andalso is_atom(Port) ->
+    start(atom_to_list(Host), binary_to_integer(atom_to_binary(Port))).
+
+start(Host, Port) when is_integer(Port) ->
+    inets:start(),
+    ssl:start(),
+    logger:set_application_level(ssl, debug),
+    SslOpts = [
+        {verify, verify_none}
+    ],
+    io:format("[INFO] ssl:versions: ~p~n", [ssl:versions()]),
+    ok = io:format("[INFO] before ssl:connect~n", []),
+    case ssl:connect(Host, Port, SslOpts) of
+        {ok, SslSocket} ->
+            ok = io:format("[INFO] after ssl:connect~n", []),
+            ok = ssl:close(SslSocket);
+        Error ->
+            ok = io:format(standard_error, "[ERROR] ssl:connect ~p~n", [Error])
+    end,
+    init:stop().
